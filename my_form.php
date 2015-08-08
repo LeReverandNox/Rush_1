793f3314f0f001ddcdae04ab2aa6cd271557d9cc
Rodolphe Laidet <lereverandnox@gmail.com>
2015-08-08 10:12:30 +0200

    Ajout de 3 champs dans le formulaire

--- my_form.php
+++ my_form.php
@@ -1,3 +1,21 @@
+<!DOCTYPE html>
 <html>
+    <head>
+        <meta charset="utf-8">
+        <title>Rush 01 - Etape 01</title>
+    </head>
+    <body>
+        <h1>Formulaire</h1>
+        <form class="formulaire" action="my_form.php" method="post">
+            <label for="sexe">Sexe : </label>
+            <input type="radio" name="sexe" value="Homme" /> Homme <br />
+            <input type="radio" name="sexe" value="Femme" /> Femme <br />
 
+            <label for="civilite">Civilité : </label>
+            <input type="select" name="civilite" value="">
+
+            <br />
+            <input type="submit" value="Envoyer">
+        </form>
+    </body>
 </html>
