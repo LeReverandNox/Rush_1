4aeced8ee33d41dd718d506edcf7e1df0c6c7ccb
LeReverandNox <lereverandnox@gmail.com>
2015-08-09 21:37:54 +0200

    Mise en place du debut de l'etape 4

---
+++ step_4/login.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js"></script>
+        <title>Login</title>
+    </head>
+    <body>
+        <h1>Veuillez vous connecter :</h1>
+        <form name="formulaire" id="formulaire" action="process.php" method="post">
+            <p><label for="username">Username : </label><input type="text" name="username" id="username" /></p>
+            <p></p><label for="password">Password : </label><input type="password" name="password" id="password" /></p>
+            <input type="submit" value="Connexion"/>
+        </form>
+    </body>
+</html>
+
+
+
+
+
---
+++ step_4/my_navigator.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Bienvenue sur le navigateur !</title>
+    </head>
+    <body>
+        <h1>Mange mon navigateur</h1>
+        <p>Hello World !</p>
+    </body>
+</html>
