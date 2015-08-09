c6bd05453ab3b090b52b445e7080079336931624
phannavong <phannavong@gmail.com>
2015-08-09 11:54:10 +0200

    Revert "champs preremplie"
    This reverts commit 53936d487011ea8b34dd9a986b75cfa92f1ad74e.

--- my_form.php
+++ my_form.php
@@ -29,15 +29,15 @@
                 <br /><br />
 
                 <label for="nom" class="no_border">Nom : </label>  <!--Label "Nom" est le nom de l'étiquette du champs de saisie  -->
-                <input type="text" name="nom" id="nom" minlength=2 placeholder="Van Damme"/> <!-- créer un champs de saisie de type text avec une zone de texte pour nom de l'élèment "nom" -->
+                <input type="text" name="nom" id="nom" minlength=2 /> <!-- créer un champs de saisie de type text avec une zone de texte pour nom de l'élèment "nom" -->
 
                 <br /><br />
 
-
+    
                 <label for="prenom" class="blue_border">Prenom : </label> <!--Label "Prénom" sur l'étiquette du champs de saisie-->
             <div class="Prenom">
-               <input type="text" name="prenom" id="prenom" minlength=2 placeholder="Magloire" />  <!-- creer un champs de saisie de type text avec une saisie de texte pour nom de l'élèment prenom -->
-            </div>
+               <input type="text" name="prenom" id="prenom" minlength=2 />  <!-- creer un champs de saisie de type text avec une saisie de texte pour nom de l'élèment prenom -->
+            </div>           
                 <br /><br />
 
                 <label for="date_de_naissance" class="no_border">Date de naissance : </label> <!-- label "date de naissance" sur l'étiquette du champs de saisie -->
@@ -50,11 +50,11 @@
                 <legend>Contacts</legend>
 
                 <label for="email" class="no_border">Email : </label> <!-- label "email" sur l'étiquette du champs de saisie  -->
-                <input type="email" name="email" id="email" placeholder="bloodsport@gmail.com" /> <!-- crée un champs de saisie de type email avec une saisie de d'adresse mail pour nom  de l'element "email" -->
+                <input type="email" name="email" id="email" /> <!-- crée un champs de saisie de type email avec une saisie de d'adresse mail pour nom  de l'element "email" -->
                 <br /><br />
 
                 <label for="telephone" class="no_border">Telephone : </label> <!-- label "telephone" sur l'étiquette du champs de saisie -->
-                <input type="tel" name="telephone" id="telephone" placeholder="01.11.82.18.40" /> <!-- crée un champs de saisie de type tel avec une saisie par numéro (affiche sur smartphone le clavier numérique ) pour nom comme element "telephone" -->
+                <input type="tel" name="telephone" id="telephone" /> <!-- crée un champs de saisie de type tel avec une saisie par numéro (affiche sur smartphone le clavier numérique ) pour nom comme element "telephone" -->
 
                 <br /><br />
 
@@ -64,11 +64,11 @@
 
                 <label for="website" class="blue_border">Site web : </label> <!-- label "telephone" sur l'étiquette du champs de saisie -->
             <div class="Site_web">
-                <input type="url" name="website" id="website" placeholder="http://jacquieetmicheltv.net" /> <!-- creer un champs de saisie de type url avec une saisie par url pour nom "website" -->
+                <input type="url" name="website" id="website" /> <!-- creer un champs de saisie de type url avec une saisie par url pour nom "website" -->
             </div>
                 <br /><br />
-
-
+                
+               
                 <label for="hobbies" class="no_border">Hobbies : </label><br />
                 <label for="jeuxvideo">Jeux video</label><input type="checkbox" name="hobbies" id="jeuxvideo" value="jeuxvideo" /> <!-- label "hobbies" sur letiquette de champs de saisie  -->       <!-- creer un champs de saisise de type  pour une case que l'on peut cocher ou décocher, sous le nom "hobbies " avec comme choix "jeuxvideo" "cinema" "lecture" "sport" "informatique". -->
 
