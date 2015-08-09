8990a4d1d7e82b5e2f5a42cb19bbad2ff9eb4ba4
LeProvotMaxime <maxleprov@gmail.com>
2015-08-09 02:32:12 +0200

    Création de la class blue_border et de la class Align sur le html Réajustement pour aligner les champs dans le fichier css

--- my_form.php
+++ my_form.php
@@ -12,14 +12,14 @@
             <fieldset>
                 <legend>Formulaire</legend>
 
-                <label for="sexe" class="no_border">Sexe : </label>   <!-- c'est une étiquette donnée au champs saisie dans ce cas l'étiquette au champs est le "sexe"(ou nous affiche le nom de du champs de saisie ) -->
+                <label for="sexe" class="blue_border">Sexe : </label>   <!-- c'est une étiquette donnée au champs saisie dans ce cas l'étiquette au champs est le "sexe"(ou nous affiche le nom de du champs de saisie ) -->
             <div id="Homme">
                 <input type="radio" name="sexe" id="homme" class="right" value="Homme" checked /><label for="homme">Homme</label> <!-- input  sert  inserer(=creer) un champs de saisie ; "type" ensuite type de saisie (radio  permet de faire SEULEMENT  un choix parmi une liste de possibilités); "Name" donne un nom de él'lèment du formulaire; "value" prépremplie le champs avec une valeur -->
                 <input type="radio" name="sexe" id="femme" class="right" value="Femme" /><label for="femme">Femme</label> <!-- inserer un champs de saisie de type radio seulement 1 choix parmi une liste défini pour nom comme element sexe avec comme choix femme -->
                 <br /><br />
             </div>
                 
-                <label for="civilite" class="no_border">Civilité : </label> <!-- civilité est le nom de l'étiquette du champs de saisie -->
+                <label for="civilite" class="blue_border">Civilité : </label> <!-- civilité est le nom de l'étiquette du champs de saisie -->
                 <select class="civilitle" name="civilite" id="civilite"> <!-- balise select c'est une liste à choix multiple pour nom civilité a l'élèment -->
                     <option value="m">M.</option>  <!-- la balise option nous donne les choix possible "M. Mme Mlle." -->
                     <option value="mme">Mme.</option>
@@ -28,32 +28,32 @@
 
                 <br /><br />
                 
-                <label for="nom" class="no_border">Nom : </label>  <!--Label "Nom" est le nom de l'étiquette du champs de saisie  -->
+                <label for="nom" class="align">Nom : </label>  <!--Label "Nom" est le nom de l'étiquette du champs de saisie  -->
                 <input type="text" name="nom" id="nom" minlength=2 /> <!-- créer un champs de saisie de type text avec une zone de texte pour nom de l'élèment "nom" -->
 
                 <br /><br />
                 
-                <label for="prenom" class="no_border">Prenom : </label> <!--Label "Prénom" sur l'étiquette du champs de saisie-->
+                <label for="prenom" class="blue_border">Prenom : </label> <!--Label "Prénom" sur l'étiquette du champs de saisie-->
             <div class="Prenom">
                <input type="text" name="prenom" id="prenom" minlength=2 />  <!-- creer un champs de saisie de type text avec une saisie de texte pour nom de l'élèment prenom -->
             </div>           
                 <br /><br />
 
-                <label for="email" class="no_border">Email : </label> <!-- label "email" sur l'étiquette du champs de saisie  -->
+                <label for="email" class="align">Email : </label> <!-- label "email" sur l'étiquette du champs de saisie  -->
                 <input type="email" name="email" id="email" /> <!-- crée un champs de saisie de type email avec une saisie de d'adresse mail pour nom  de l'element "email" -->
                 <br /><br />
-                <label for="telephone" class="no_border">Telephone : </label> <!-- label "telephone" sur l'étiquette du champs de saisie -->
+                <label for="telephone" class="align">Telephone : </label> <!-- label "telephone" sur l'étiquette du champs de saisie -->
                 <input type="tel" name="telephone" id="telephone" /> <!-- crée un champs de saisie de type tel avec une saisie par numéro (affiche sur smartphone le clavier numérique ) pour nom comme element "telephone" -->
 
                 <br /><br />
                 
-                <label for="website" class="no_border">Site web : </label> <!-- label "telephone" sur l'étiquette du champs de saisie -->
+                <label for="website" class="blue_border">Site web : </label> <!-- label "telephone" sur l'étiquette du champs de saisie -->
             <div class="Site_web">
                 <input type="url" name="website" id="website" /> <!-- creer un champs de saisie de type url avec une saisie par url pour nom "website" -->
             </div>
                 <br /><br />
                 
-                <label for="date_de_naissance" class="no_border">Date de naissance : </label> <!-- label "date de naissance" sur l'étiquette du champs de saisie -->
+                <label for="date_de_naissance" class="align">Date de naissance : </label> <!-- label "date de naissance" sur l'étiquette du champs de saisie -->
                 <input type="date" name="date_de_naissance" id="date_de_naissance" /> <!-- creer un champs de saisie de type date avec une saisie ../../.... ou sous forme de calendrier  -->
 
                 <br /><br />
